d14fd0641582802952a06767f37946ba7f8ada8b
lolli <lolli@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-30 19:08:51 +0000

    Raised version to 0.2.0, added dependency to TYPO3 4.5
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/enetcacheanalytics/trunk@46002 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,14 +29,15 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.0',
+	'version' => '0.2.0',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '4.5.0-4.6.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'enetcache' => '0.0.0',
+			'enetcache' => '1.0.1',
 		),
 	),
 	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"f0a7";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"ec3e";s:19:"doc/wizard_form.dat";s:4:"9dc6";s:20:"doc/wizard_form.html";s:4:"52c1";s:13:"mod1/conf.php";s:4:"b00b";s:14:"mod1/index.php";s:4:"23d1";s:18:"mod1/locallang.xml";s:4:"50b4";s:22:"mod1/locallang_mod.xml";s:4:"003e";s:22:"mod1/mod_template.html";s:4:"7c59";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
